ffa4f7ac1d9186e784c5f4e87c3b88be35ca9f24
Evgeny Cherkashin <eugeneai@irnok.net>
2013-10-30 15:57:38 +0900

    Vector functions now return correct columns and matrices.

--- paropt.py
+++ paropt.py
@@ -4,7 +4,7 @@ from numpy import *
 from functools import reduce
 from pprint import pprint
 import itertools
-from sympy import symbols, diff
+from sympy import symbols, diff, Symbol
 
 #DEBUG = 20
 DEBUG = 5
@@ -21,50 +21,108 @@ def %s(self, t, x, u):
 
     print (s, f)
 
-def _comp(exp):
-    return compile(str(exp), "<-%s->" % str(exp), "eval")
+def _vcomp(exp):
+    sexp="array(%s)" % str(exp)
+    return compile(sexp, "<-%s->" % sexp, "eval")
 
-def _eval(f, t, X, U):
-    rc = eval(f, {'t':t, 'x':X, 'u':U, 'array':array})
+def _comp(exp):
+    sexp=str(exp)
+    return compile(sexp, "<-%s->" % sexp, "eval")
+
+def _eval(f, t, xc, uc, V):
+    g={'t':t, 'array':array}
+    for i, x in enumerate(V.x[0]):
+        g.setdefault(str(x),xc[0][i])
+    for i, u in enumerate(V.u[0]):
+        g.setdefault(str(u),uc[0][i])
+
+    rc = eval(f, g)
+    return rc
+    """
     try:
         rc[0]
         return rc
     except TypeError:
         return [array([rc]) for _ in t]
+    """
 
-def _diff(f_col, v, num=1):
+def _vdiff(F, V, num=1):
     answer=[]
-    print (f_col, v)
-    for f in f_col:
-        f = f[0]
+    print (F, V)
+    DF=[]
+
+    for f in F[0]:
+        dfs=[]
+        for v in V[0]:
+            df=diff(f, v, num)
+            dfs.append(df)
+        DF.append(dfs)
+    print ("D:", DF)
+    return DF
+
+def _diff(f, V, num=1):
+    answer=[]
+    print (f, V)
+    DF=[]
+
+    dfs=[]
+    for v in V[0]:
         df=diff(f, v, num)
-        answer.append([df])
-    print ("D:", answer)
-    return str(answer)
-    #return "array("+str(answer)+")"
+        dfs.append(df)
+    DF.append(dfs)
+    print ("D:", DF)
+    return DF
+
+def _teval(d, T,X,U, V): # d is a code of function to be evaluated at all T time instance.
+    rc=[]
+    for t in T:
+        rc.append(_eval(d, t, X[t], U[t], V))
+    return rc
+
+
+class Helper():
+    pass
 
 class ParOptModel(object):
     """
     """
 
-    def __init__(self, X0):
+    def __init__(self, X0, N, M):
         """
         """
+        self.N=N     # Dimention of x
+        self.M=M     # Dimention of u
         self.X0=X0
-        t, x, u = symbols("t, x, u")
-        print (t, x, u)
+        self.v=Helper()
+        self.v.t=Symbol('t')
+        a=[]
+        for i in range(N):
+            a.append(Symbol('x%s' % i))
+        self.v.x=[a]
+        a=[]
+        for i in range(M):
+            a.append(Symbol('u%s' % i))
+        self.v.u=[a]
+
+        t, x, u = self.v.t, self.v.x, self.v.u
+
         self._f=self.f(t, x, u)
         self._f0=self.f0(t, x, u)
         self._F=self.F(x)
-        self._dfdx=_comp(_diff(self._f, x))
-        self._df0dx=_comp(_diff(self._f0, x))
-        self._dFdx=_comp(_diff(self._F, x))
 
-        self._dfdu=_comp(_diff(self._f, u))
-        self._df0du=_comp(_diff(self._f0, u))
-        #self.dfdu=compile_method("dfdu", self._dfdu)
+        # -----------------
+        self._dfdx=_vcomp(_vdiff(self._f, x))
+        self._df0dx=_vcomp(_diff(self._f0, x))
+        self._dFdx=_vcomp(_diff(self._F, x))
+
+        self._dfdu=_vcomp(_vdiff(self._f, u))
+        self._df0du=_vcomp(_diff(self._f0, u))
+
         if DEBUG>=5:
             print (self._f, self._f0, self._dfdx, self._df0dx, sep=", ")
+
+        #self.dfdu=compile_method("dfdu", self._dfdu)
+        if DEBUG>=5:
             print (self._F, self._dFdx, sep=", ")
             print (self._f, self._f0, self._dfdu, self._df0du, sep=", ")
 
@@ -87,22 +145,24 @@ class ParOptModel(object):
     def dFdx(self, xe):
         return eval(self._dFdx, {'x':xe, 'array':array})
 
-    def dfdx(self, t, X, U, dt=1):
-        return _eval(self._dfdx, t, X, U)
+    def dfdx(self, T, X, U, dt=1):
+        return _teval(self._dfdx,  T,X,U, self.v)
 
-    def df0dx(self, t, X, U, dt=1):
-        return _eval(self._df0dx, t, X, U)
+    def df0dx(self, T, X, U, dt=1):
+        return _teval(self._df0dx, T,X,U, self.v)
 
-    def dfdu(self, t, X, U, dt=1):
-        return _eval(self._dfdu, t, X, U)
+    def dfdu(self, T, X, U, dt=1):
+        return _teval(self._dfdu, T,X,U, self.v)
 
-    def df0du(self, t, X, U, dt=1):
-        return _eval(self._df0du, t, X, U)
+    def df0du(self, T, X, U, dt=1):
+        return _teval(self._df0du, T,X,U, self.v)
 
     def Psi(self, t, X, U, alpha):
 
         psie = -self.dFdx(X[-1])
+
         psi=[psie]
+
         _df0dx=self.df0dx(t, X, U) # last element is useless
         _dfdx =self.dfdx (t, X, U) # last element is useless
 
@@ -232,7 +292,9 @@ class ParOptProcess(object):
             beta = self.beta
             while True:
                 (Xn, Un) = self.improve(t, Xp, Up, beta=beta)
+#                import pdb; pdb.set_trace()
                 In = self.model.I(Xn, Un)
+
                 dI = Ip-In
                 print ("DI:", dI)
                 #print ("Xn:", Xn)
@@ -255,7 +317,6 @@ class ParOptProcess(object):
 
     def trajectory(self, U):
 
-        import pudb; pu.db
         x0=self.model.X0
         X = [x0]
 
@@ -337,27 +398,33 @@ class LinModel1(ParOptModel):
             num=self.num
         )
         self.t = arange(len(self.T))
-        ParOptModel.__init__(self, X0=X0)
+        ParOptModel.__init__(self, X0=X0, N=1, M=1)
 
     def start_control(self):
-        U = [array([0.0]) for t in self.t]
+        U = [array([[0.0]]) for t in self.t]
         return array(U)
 
     def F(self, x):
         """ X and U are lists of vectors (arrays)
         """
-        return [[0.0]]
+        return 0.0
 
     def f(self, t, x, u, dt=1):
         """ X ia a vector of the previous state
         """
-        return [[x+self.h*u]]
+        ((x0,),)=x
+        ((u0,),)=u
+
+        return [[x0+self.h*u0]]
 
 
     def f0(self, t, x, u, dt=1):
         """ X ia a vector of the previous state
         """
-        return [[self.h * (x*x+u*u)]] #reduce(lambda a, e: a+e[0]**2+e[1]**2, list(zip(X,U))[:-1], 0.0)
+        ((x0,),)=x
+        ((u0,),)=u
+
+        return self.h * (x0*x0+u0*u0)
 
 
 class LinModel2(LinModel1):
